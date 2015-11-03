8ce64f49c01bffb9d3dc9c5564fc1e6299cf0bbb
Paweł Stiasny <pawelstiasny@gmail.com>
2015-11-03 22:01:55 +0100

    update tests

--- test_path_ops.py
+++ test_path_ops.py
@@ -1,18 +1,32 @@
 #!/usr/bin/py.test
 
+import pytest
+
 from main import *
 
 
-def test_swapping_inner_edges():
-    a = [0, 1, 2, 3, 4]
-    assert swap_edges(a, 1, 3) == [0, 3, 2, 1, 4]
+@pytest.fixture
+def graph():
+    return Graph(['A', 'B', 'C', 'D', 'E'],
+                 [[0, 1, 2, 3, 4],
+                  [1, 0, 1, 2, 3],
+                  [2, 1, 0, 1, 2],
+                  [3, 2, 1, 0, 1],
+                  [4, 3, 2, 1, 0]])
 
 
-def test_swapping_late_edges():
-    a = [0, 1, 2, 3, 4]
-    assert swap_edges(a, 3, 4) == [0, 1, 2, 4, 3]
+@pytest.fixture
+def route(graph):
+    return Route(graph, [0, 1, 2, 3, 4])
 
 
-def test_swapping_early_edges():
-    a = [0, 1, 2, 3, 4]
-    assert swap_edges(a, 0, 2) == [2, 1, 0, 3, 4]
+def test_swapping_inner_edges(route):
+    assert route.swap_edges(1, 3).nodes == [0, 3, 2, 1, 4]
+
+
+def test_swapping_late_edges(route):
+    assert route.swap_edges(3, 4).nodes == [0, 1, 2, 4, 3]
+
+
+def test_swapping_early_edges(route):
+    assert route.swap_edges(0, 2).nodes == [2, 1, 0, 3, 4]
